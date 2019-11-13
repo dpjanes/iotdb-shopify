0d2512f8f68c1da48591276d3ad0616016dcffb4
David Janes <david@consensas.com>
2019-11-13 09:51:50 -0500

    roughin

---
+++ test/initialize.js
@@ -0,0 +1,52 @@
+/**
+ *  test/initialize.js
+ *
+ *  David Janes
+ *  IOTDB
+ *  2019-11-13
+ *
+ *  Copyright (2013-2019-11-13
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+"use strict"
+
+const _ = require("iotdb-helpers")
+
+const assert = require("assert")
+
+const shopify = require("..")
+const _util = require("./_util")
+
+describe("initialize", function() {
+    let self = {}
+
+    before(function(done) {
+        _.promise(self)
+            .then(_util.initialize)
+            .then(_util.load)
+            .make(sd => {
+                self = sd
+            })
+            .then(_.promise.done(done))
+            .catch(done)
+    })
+
+    describe("good", function() {
+        it("works", function(done) {
+            _.promise(self)
+                .end(done)
+        })
+    })
+})
